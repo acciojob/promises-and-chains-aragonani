30b4f50fe1c1038d12d6785d51df9db55aa90160
aragonani <101066123+aragonani@users.noreply.github.com>
2025-05-01 13:23:20 -0700

    created a submission

--- script.js
+++ script.js
@@ -15,9 +15,9 @@
       new Promise((resolve, reject) => {
         setTimeout(() => {
           if (age > 18) {
-            resolve(`Welcome, ${name}. You can vote.`);
+            resolve('Welcome, . You can vote.');
           } else {
-            reject(`Oh sorry ${name}. You aren't old enough.`);
+            reject("Oh sorry . You aren't old enough.");
           }
         }, 4000); // 4-second delay
       })
